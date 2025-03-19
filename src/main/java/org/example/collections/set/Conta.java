356e4de4a74f9dca11399228afbc572a4dc574fb
matera-sgc <samuel.cunha@matera.com>
2025-03-19 20:30:03 -0300

    Aula sobre Listas List e Set

---
+++ src/main/java/org/example/collections/set/Conta.java
@@ -0,0 +1,23 @@
+package org.example.collections.set;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Conta {
+
+    private Long numeroConta;
+    private Double saldo;
+
+    @Override
+    public int hashCode() {
+        return numeroConta.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Conta conta = (Conta) obj;
+        return this.numeroConta.equals(conta.getNumeroConta());
+    }
+}
